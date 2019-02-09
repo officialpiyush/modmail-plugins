9259a5d0698c850aaaef1b06b6bd8ffde128ac8b
ionadev <bhangalepiyush@gmail.com>
2019-02-09 19:54:18 +0530

    Basic tags Plugin Cmmit

---
+++ Tags/tags.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class TagPlugin:
+    def __init__(self,bot):
+        self.bot = bot
+        self.db = bot.plugin_db.get_partition(self)
+
+    @commands.command()
+    async def info(self,ctx):
+        await ctx.send('A Plugin Created to server your need for tags!')
+    
+    @commands.group()
+    async def tags(self,ctx):
+        if ctx.invoked_subcommand is None:
+            cmd = self.bot.get_command('help')
+        await ctx.invoke(cmd,command="tags")
+    
+    @tags.command(name="add")
+    async def add_(self,ctx,*,message: commands.clean_content):
+       message = message.split("||")
+       print(message)
+       #tag = await self.db.find_one({'tagName': message[0]})
