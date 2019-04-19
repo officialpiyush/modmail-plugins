0eb93c7897e1b532531a1f89848a4046e86bea7a
piyush <bhangalepiyush@gmail.com>
2019-04-19 15:29:28 +0530

    Hastebin Cog

---
+++ hastebin/hastebin.py
@@ -0,0 +1,36 @@
+import discord
+import os
+from discord import Embed
+from discord.ext import commands
+
+from json import JSONDecodeError
+from aiohttp import ClientResponseError
+
+class HastebinCog:
+    def __init__(self,bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def hastebin(self,ctx,*,message):
+        """Upload Text To hastebin"""
+        haste_url = os.environ.get('HASTE_URL', 'https://hasteb.in')
+
+        try:
+            async with self.bot.session.post(haste_url + '/documents',data=message) as resp:
+                key = (await resp.json())["key"]
+                embed = Embed(
+                    title='Your Uploaded File',
+                    color=self.bot.main_color,
+                    description=f'{haste_url}/' + key
+                )
+        except (JSONDecodeError, ClientResponseError, IndexError):
+            embed = Embed(
+                color=self.bot.main_color,
+                description='Something\'s wrong. '
+                            'We\'re unable to upload your text to hastebin.'
+            )
+            embed.set_footer(text='Hastebin Plugin')
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(HastebinCog(bot))
