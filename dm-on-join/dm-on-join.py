a772f80b318bd87bf35f8db30e5513216a9c43ce
Piyush <bhangalepiyush@gmail.com>
2019-05-05 13:22:55 +0530

    dm-on-koin

---
+++ dm-on-join/dm-on-join.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+class DmOnJoinPlugin:
+    def __init__(self,bot):
+        self.bot = bot
+        self.db = bot.plugin_db.get_partition(self)
+    
+    @commands.command(aliases=["sdms"])
+    @commands.check(manage_server=True)
+    async def setdmmessage(self,ctx,*,message):
+        """Set A Message To DM A user after they join."""
+        await self.db.find_one_and_update(
+        {'_id': 'dm-config'},
+        {'$set': {'dm-message': {'message': message}}},
+        upsert=True
+        )
+
+        await ctx.send("Successfully Set The Message.")
+    
+    async def on_member_join(self,member):
+        config = (await self.db.find_one({'_id': 'dm-config'}))['dm-message']
+        if config is None:
+            return
+        else:
+            try:
+                member.send(config["message"])
+            except: 
+                return
+    
+def setup(bot):
+    bot.add_cog(DmOnJoinPlugin(bot))
