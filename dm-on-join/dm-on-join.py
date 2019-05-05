cd34a81017f39c21ff43c4774ba1290e0237d575
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-05-05 16:40:52 +0530

    Admin

--- dm-on-join/dm-on-join.py
+++ dm-on-join/dm-on-join.py
@@ -7,7 +7,7 @@ class DmOnJoinPlugin:
         self.db = bot.plugin_db.get_partition(self)
     
     @commands.command(aliases=["sdms"])
-    @commands.check(manage_server=True)
+    @commands.check(administrator=True)
     async def setdmmessage(self,ctx,*,message):
         """Set A Message To DM A user after they join."""
         await self.db.find_one_and_update(
