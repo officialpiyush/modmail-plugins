3cf79c4dbc56b2e10ec291201334c26b93a5efa6
Piyush <bhangalepiyush@gmail.com>
2019-05-06 21:22:19 +0530

    Translate text Messages - Closes #2

--- translator/translator.py
+++ translator/translator.py
@@ -14,17 +14,28 @@ class TranslatePlugin:
         try:
             msg = await ctx.channel.get_message(msgid)
             if not msg.embeds:
-                await ctx.send("Couldnt Find An Embed in that message.")
+                ms = msg.content
+            else:
+                ms = msg.embeds[0].description
             
             embed = msg.embeds[0]
-            tmsg = self.translator.translate(embed.description)
-            rembed = discord.Embed()
-            rembed.description = tmsg.text
-            await ctx.channel.send(embed=rembed)
+            tmsg = self.translator.translate(ms)
+            embed = discord.Embed()
+            embed.color = 4388013
+            embed.description = tmsg.text
+            await ctx.channel.send(embed=embed)
         except NotFound:
             await ctx.send("The Given Message Was Not Found.")
         except HTTPException:
             await ctx.send("The Try To Retrieve The Message Failed.")
+
+    @commands.command(aliases=["tt"])
+    async def translatetext(self,ctx,*,message):
+        tmsg = self.translator.translate(message)
+        embed = discord.Embed()
+        embed.color = 4388013
+        embed.description = tmsg.text
+        await ctx.channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(TranslatePlugin(bot))
