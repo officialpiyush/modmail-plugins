4ccb7dda90eb381d01c9e77e2aa09f6539f63571
Piyush <bhangalepiyush@gmail.com>
2019-05-07 18:25:54 +0530

    Fixes

--- translator/translator.py
+++ translator/translator.py
@@ -13,14 +13,14 @@ class TranslatePlugin:
         self.tt = set()
         self.enabled = True
         asyncio.create_task(self._set_config())
-    
+
     async def _set_config(self):
         config = await self.db.find_one({'_id': 'config'})
-        self.enabled = config.get('at-enabled', True)
-        self.tt = set(config.get('auto-translate', []))
+        self.enabled = config.get('enabled', True)
+        self.tt = set(config.get('translateSet', []))
 
     @commands.command()
-    async def translate(self,ctx,msgid: int):
+    async def translate(self, ctx, msgid: int):
         """Translate A Sent Message, or a modmail thread message into English."""
         try:
             msg = await ctx.channel.get_message(msgid)
@@ -28,7 +28,7 @@ class TranslatePlugin:
                 ms = msg.content
             else:
                 ms = msg.embeds[0].description
-            
+
             embed = msg.embeds[0]
             tmsg = self.translator.translate(ms)
             embed = discord.Embed()
@@ -41,7 +41,7 @@ class TranslatePlugin:
             await ctx.send("The Try To Retrieve The Message Failed.")
 
     @commands.command(aliases=["tt"])
-    async def translatetext(self,ctx,*,message):
+    async def translatetext(self, ctx, *, message):
         """Translates Given Message Into English"""
         tmsg = self.translator.translate(message)
         embed = discord.Embed()
@@ -51,7 +51,7 @@ class TranslatePlugin:
 
     @commands.command(aliases=["att"])
     @commands.has_permissions(manage_messages=True)
-    async def auto_translate_thread(self,ctx):
+    async def auto_translate_thread(self, ctx):
         if "User ID:" not in ctx.channel.topic:
             await ctx.send("The Channel Is Not A Modmail Thread")
             return
@@ -61,53 +61,62 @@ class TranslatePlugin:
         else:
             self.tt.add(ctx.channel.id)
             removed = False
-        
+
         await self.db.update_one(
             {'_id': 'config'},
-            {'$set': {'auto-translate': list(self.tt)}}, 
+            {'$set': {'translateSet': list(self.tt)}},
             upsert=True
-            )
+        )
 
         await ctx.send(f"{'Removed' if removed else 'Added'} Channel {'from' if removed else 'to'} Auto Translations List.")
-    
+
     @commands.command(aliases=["tat"])
     @commands.has_permissions(manage_guild=True)
-    async def toggle_auto_translations(self,ctx, enabled: bool):
+    async def toggle_auto_translations(self, ctx, enabled: bool):
         self.enabled = enabled
-        await self.coll.update_one(
+        await self.db.update_one(
             {'_id': 'config'},
-            {'$set': {'at-enabled': self.enabled}}, 
+            {'$set': {'enabled': self.enabled}},
             upsert=True
-            )
+        )
         await ctx.send(f"{'Enabled' if enabled else 'Disabled'} Auto Translations")
-    
-    async def on_message(self,message):
+
+    async def on_message(self, message):
         if not self.enabled:
             return
-        
+
         channel = message.channel
 
         if channel.id not in self.tt:
             return
-        
-        if isinstance(message.author,User):
+
+        if isinstance(message.author, User):
             return
-        
+
         if "User ID:" not in channel.topic:
             return
-        
+
         if not message.embeds:
             return
-        
+
         msg = message.embeds[0].description
-        tmsg = self.translator.translate(msg)       
+        tmsg = self.translator.translate(msg)
         embed = discord.Embed()
         embed.description = tmsg.text
         embed.color = 4388013
-        embed.footer = "Auto Translator Plugin"
+        embed.set_footer = "Auto Translator Plugin"
 
         await channel.send(embed=embed)
 
+    async def on_ready(self):
+        config = await self.db.find_one({'_id': 'config'})
+
+        if config is None:
+            await self.db.find_one_and_update(
+                {'_id': 'config'},
+                {'$set': {'enabled': True, 'translateSet': list([])}},
+                upsert=True
+            )
 
 def setup(bot):
     bot.add_cog(TranslatePlugin(bot))
