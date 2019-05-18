fffde53f233bdd0334589fb66c53da1914a5e6dc
Piyush <bhangalepiyush@gmail.com>
2019-05-18 13:36:38 +0530

    Reboot Plugin

---
+++ reboot/reboot.py
@@ -0,0 +1,19 @@
+import sys
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+
+class RebootCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.OWNER)
+    async def reboot(self, ctx):
+        return sys.exit()
+
+
+def setup(bot):
+    bot.add_cog(RebootCog(bot))
