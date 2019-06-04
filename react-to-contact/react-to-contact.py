67d086a3d98a1367f3be09ace907cdffff52553a
Piyush <bhangalepiyush@gmail.com>
2019-06-04 19:56:12 +0530

    code cleanup

--- react-to-contact/react-to-contact.py
+++ react-to-contact/react-to-contact.py
@@ -59,10 +59,7 @@ class ReactToContact(commands.Cog):
                 " `(This Reaction Should be added on the message previously or it won't work.)`"
             )
             reaction, user = await self.bot.wait_for("reaction_add", check=check)
-            # if isinstance(reaction.emoji, str):
-            #     react = reaction.emoji
-            # elif isinstance(reaction.emoji, discord.Emoji):
-            #     react = reaction.emoji.name
+
             await self.db.find_one_and_update(
                 {"_id": "config"},
                 {
