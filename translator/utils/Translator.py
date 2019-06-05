b58123f1dc81c180dd284b24d74593821ae7692e
Piyush <bhangalepiyush@gmail.com>
2019-06-05 11:01:45 +0530

    Translator class - basic

---
+++ translator/utils/Translator.py
@@ -0,0 +1,8 @@
+from googletrans import Translator as CoreTranslator
+
+class Translator:
+    def __init__(self):
+        self.t = CoreTranslator()
+    
+    def translate(self, msg: str, dest: str = en):
+        return CoreTranslator.translate(msg,dest=dest)
