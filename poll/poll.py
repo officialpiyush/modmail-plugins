1098c2a3adfc8be990326900f475eef75c19d06f
DAzVise <52792999+DAzVise@users.noreply.github.com>
2019-10-19 20:15:26 +0300

    plugin(poll): Enhancements
    * format 2.0
    
    * Update poll.py
    
    * typo
    
    * yikes
    
    * fix
    
    * emoji
    
    * Update poll.py
    
    * Update poll.py
    
    * am nub
    
    * Update poll.py

--- poll/poll.py
+++ poll/poll.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2015 Rapptz
 from discord.ext import commands
+import discord
 import asyncio
+import datetime
 
 from core import checks
 from core.models import PermissionLevel
@@ -16,15 +18,28 @@ class Polls(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
+    
+   
+    @commands.group(name="poll", invoke_without_command=True)
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    async def poll(self, ctx: commands.Context):
+        """Easily create Polls.
+        """
+        await ctx.send_help(ctx.command)
+        
+    @poll.command()
     @commands.guild_only()
-    @checks.has_permissions(PermissionLevel.ADMIN)
-    async def poll(self, ctx, *, question):
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    async def start(self, ctx, *, question):
         """Interactively creates a poll with the following question.
 
         To vote, use reactions!
         """
+        perms = ctx.channel.permissions_for(ctx.me)
+        if not perms.add_reactions:
+            return await ctx.send(
+                "Need Add Reactions permissions."
+            )
 
         # a list of messages to delete when we're all done
         messages = [ctx.message]
@@ -40,7 +55,7 @@ class Polls(commands.Cog):
         for i in range(20):
             messages.append(
                 await ctx.send(
-                    f"Say poll option or {ctx.prefix}cancel to publish poll."
+                    f"Say a Poll option or {ctx.prefix}done to publish the Poll."
                 )
             )
 
@@ -51,7 +66,7 @@ class Polls(commands.Cog):
 
             messages.append(entry)
 
-            if entry.clean_content.startswith(f"{ctx.prefix}cancel"):
+            if entry.clean_content.startswith(f"{ctx.prefix}done"):
                 break
 
             answers.append((to_emoji(i), entry.clean_content))
@@ -60,50 +75,67 @@ class Polls(commands.Cog):
             await ctx.channel.delete_messages(messages)
         except:
             pass  # oh well
-
+        
         answer = "\n".join(f"{keycap}: {content}" for keycap, content in answers)
-        actual_poll = await ctx.send(f"{ctx.author} asks: {question}\n\n{answer}")
+        embed = discord.Embed(color=self.bot.main_color, timestamp=datetime.datetime.utcnow(), description=f"**{question}**\n{answer}")
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        poll = await ctx.send(embed=embed)
         for emoji, _ in answers:
-            await actual_poll.add_reaction(emoji)
+            await poll.add_reaction(emoji)
 
-    @poll.error
+    @start.error
     async def poll_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.send("Missing the question.")
 
-    @commands.command()
+    @poll.command()
     @commands.guild_only()
-    @checks.has_permissions(PermissionLevel.ADMIN)
-    async def quickpoll(self, ctx, *questions_and_choices: str):
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    async def quick(self, ctx, *questions_and_choices: str):
         """Makes a poll quickly.
-
         The first argument is the question and the rest are the choices.
+        for example: `?poll quick "Green or Light Green?" Green "Light Green"`
+        
+        or it can be a simple yes or no poll, like:
+        `?poll quick "Do you watch Anime?"`
         """
 
-        if len(questions_and_choices) < 3:
-            return await ctx.send("Need at least 1 question with 2 choices.")
+        if len(questions_and_choices) == 0:
+            return await ctx.send("You need to specify a question.")
+        elif len(questions_and_choices) == 2:
+            return await ctx.send("You need at least 2 choices.")
         elif len(questions_and_choices) > 21:
             return await ctx.send("You can only have up to 20 choices.")
 
         perms = ctx.channel.permissions_for(ctx.me)
-        if not (perms.read_message_history or perms.add_reactions):
+        if not perms.add_reactions:
             return await ctx.send(
-                "Need Read Message History and Add Reactions permissions."
+                "Need Add Reactions permissions."
             )
-
-        question = questions_and_choices[0]
-        choices = [(to_emoji(e), v) for e, v in enumerate(questions_and_choices[1:])]
-
         try:
             await ctx.message.delete()
         except:
             pass
+        question = questions_and_choices[0]
+        
+        if len(questions_and_choices) == 1:
+            embed = discord.Embed(color=self.bot.main_color, description=f"**{question}**")
+            embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+            poll = await ctx.send(embed=embed)
+            reactions = ["👍", "👎"]
+            for emoji in reactions:
+                await poll.add_reaction(emoji)
+                   
+        else:
+            choices = [(to_emoji(e), v) for e, v in enumerate(questions_and_choices[1:])]
 
-        body = "\n".join(f"{key}: {c}" for key, c in choices)
-        poll = await ctx.send(f"{ctx.author} asks: {question}\n\n{body}")
-        for emoji, _ in choices:
-            await poll.add_reaction(emoji)
+            body = "\n".join(f"{key}: {c}" for key, c in choices)
+            embed = discord.Embed(color=self.bot.main_color, timestamp=datetime.datetime.utcnow(), description=f"**{question}**\n{body}")
+            embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+            poll = await ctx.send(embed=embed)
+            for emoji, _ in choices:
+                await poll.add_reaction(emoji)
 
-
+        
 def setup(bot):
     bot.add_cog(Polls(bot))
