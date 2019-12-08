450a50ce2b9de9a07f706d2c518a521ac1e5d5eb
unknown <bhangalepiyush@gmail.com>
2019-12-08 13:10:30 +0530

    GiveawayPlugin: refractor

--- giveaway/giveaway.py
+++ giveaway/giveaway.py
@@ -47,7 +47,7 @@ class GiveawayPlugin(commands.Cog):
         async def get_random_user(users, _guild, _winners):
             rnd = random.choice(users)
             in_guild = _guild.get_member(rnd.id)
-            if rnd not in _winners and in_guild is not None and in_guild != guild.me:
+            if rnd not in _winners and in_guild is not None and in_guild != _guild.me:
                 _winners.append(rnd)
                 return _winners
             else:
